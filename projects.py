6e49f6f971c9abdb02f5b3cfbf20e87ef85b244c
janeetazahid <55668993+janeetazahid@users.noreply.github.com>
2020-12-12 20:37:23 -0500

    "adding file"

--- projects.py
+++ projects.py
@@ -6,12 +6,13 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 import lightgbm as lgb
 from sklearn import metrics 
+import category_encoders as ce 
 
 
 projects_df=pd.read_csv('ks-projects-201801.csv',parse_dates=['deadline', 'launched'])
-print(projects_df.head(5))
+#print(projects_df.head(5))
 # print all of the columns 
-print(projects_df.columns)
+#print(projects_df.columns)
 # we want to predict the "state" column
 
 ## Data cleaning 
@@ -34,7 +35,7 @@ encoded=projects_df[categorical_features].apply(encoder.fit_transform)
 
 #create a new dataset with the encoded data and use it to train the model
 data=projects_df[['goal','hour','day','month','year','outcome']].join(encoded)
-print(data.head())
+#print(data.head())
 
 #split the data into training, testing and validation sets 
 valid_fraction=0.1
@@ -62,4 +63,22 @@ bst=lgb.train(param,dtrain,num_rounds,valid_sets=[dvalid],early_stopping_rounds=
 y_pred=bst.predict(test[features_cols])
 score=metrics.roc_auc_score(test['outcome'],y_pred)
 
-print("Test AUC Score{}".format(score))
+#print("Test AUC Score{}".format(score))
+
+#try applying count encoding
+count_encoder=ce.CountEncoder()
+#train a new model with count encoded data
+count_encoded=count_encoder.fit_transform(projects_df[categorical_features])
+data_2=data.join(count_encoded.add_suffix("_count"))
+train_2=data_2[:-2*valid_size]
+test_2=data_2[-valid_size:]
+valid_2=data_2[-2*valid_size:-valid_size]
+
+dtrain_2=lgb.Dataset(train_2[features_cols],label=train_2['outcome'])
+dvalid_2=lgb.Dataset(valid_2[features_cols],label=valid_2['outcome'])
+bst_2=lgb.train(param,dtrain_2,num_boost_round=1000,valid_sets=[dvalid_2],early_stopping_rounds=10,verbose_eval=False)
+
+valid_pred=bst_2.predict(valid_2[features_cols])
+valid_score=metrics.roc_auc_score(valid_2['outcome'],valid_pred)
+
+print("Validation AUC score: {}".format(valid_score))
